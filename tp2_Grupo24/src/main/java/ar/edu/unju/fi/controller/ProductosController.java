6e42cbf745dcf52038ef253533e769faf3c215d5
David <David@DESKTOP-MHN5J54>
2023-05-17 18:09:55 -0300

    Se desarrollo el controller y los html producto, para poder crear un nuevo producto

--- tp2_Grupo24/src/main/java/ar/edu/unju/fi/controller/ProductosController.java
+++ tp2_Grupo24/src/main/java/ar/edu/unju/fi/controller/ProductosController.java
@@ -3,12 +3,13 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import ar.edu.unju.fi.lista.ListaProducto;
-import ar.edu.unju.fi.model.Sucursal;
+import ar.edu.unju.fi.model.Producto;
+
 
 @Controller
 @RequestMapping("/producto")
@@ -21,9 +22,14 @@ public class ProductosController {
 	}
 	@GetMapping("/nuevo")
 	public String getNuevaSucursalPage(Model model) {
-		model.addAttribute("productos", new Sucursal());
+		model.addAttribute("producto", new Producto());
 		return "nuevo_producto";
 	}
-	
-	
+	@PostMapping("/guardar")
+	public ModelAndView setNuevaSucursal(@ModelAttribute("producto")Producto producto) {
+		ModelAndView modelView = new ModelAndView("productos");
+		listaProducto.getProductos().add(producto);
+		modelView.addObject("productos",listaProducto.getProductos());
+		return modelView;
+	}
 }
