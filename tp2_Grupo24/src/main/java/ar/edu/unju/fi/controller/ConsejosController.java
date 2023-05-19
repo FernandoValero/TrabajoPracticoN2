c0e9fcafeda446e73de4901e8192fe96d46f75fd
eveli <eveli@DESKTOP-FMF8N0N>
2023-05-19 14:53:40 -0300

    Diseño de formulario

--- tp2_Grupo24/src/main/java/ar/edu/unju/fi/controller/ConsejosController.java
+++ tp2_Grupo24/src/main/java/ar/edu/unju/fi/controller/ConsejosController.java
@@ -1,14 +1,20 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/consejos")
 public class ConsejosController {
 	
-	@GetMapping("/consejos")
-	public String getPaginaConsejos() {
-		return "consejos";
+	@GetMapping(value = "/consejos")
+	public String getPaginaConsejo(Model model) {
+	    return "consejos";
 	}
 	
+
+
 }
