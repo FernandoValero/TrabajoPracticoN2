b7c1304c6b44cc6cd3d9def40d2e4bae01946a50
David <David@DESKTOP-MHN5J54>
2023-05-19 22:08:11 -0300

    Permite agragar Productos con imágenes predefinidas

--- tp2_Grupo24/src/main/java/ar/edu/unju/fi/model/Producto.java
+++ tp2_Grupo24/src/main/java/ar/edu/unju/fi/model/Producto.java
@@ -6,46 +6,82 @@ public class Producto {
 	private double precio;
 	private String categoria;
 	private int descuento;
+	private String imagen;
 	public Producto() {
 		// TODO Auto-generated constructor stub
 	}
-	public Producto(String nombre, int codigo, double precio, String categoria, int descuento) {
+	
+	
+	public Producto(String nombre, int codigo, double precio, String categoria, int descuento, String imagen) {
 		this.nombre = nombre;
 		this.codigo = codigo;
 		this.precio = precio;
 		this.categoria = categoria;
 		this.descuento = descuento;
+		this.imagen = imagen;
 	}
+
+
 	public String getNombre() {
 		return nombre;
 	}
+
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+
 	public int getCodigo() {
 		return codigo;
 	}
+
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
+
 	public double getPrecio() {
 		return precio;
 	}
+
+
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
+
+
 	public String getCategoria() {
 		return categoria;
 	}
+
+
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
+
+
 	public int getDescuento() {
 		return descuento;
 	}
+
+
 	public void setDescuento(int descuento) {
 		this.descuento = descuento;
 	}
+
+
+	public String getImagen() {
+		return imagen;
+	}
+
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+
 	public double calcularDescuento() {
 		return precio-(precio*descuento/100);
 	}
