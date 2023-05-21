10871ce7b1348db62e8b91e219bef3e9140a38b1
NestorCorimayo <NestorCorimayo@DESKTOP-624CO4I>
2023-05-21 00:14:26 -0300

    Desarrollo completo de CRUD de Servicios.

---
+++ tp2_Grupo24/src/main/java/ar/edu/unju/fi/lista/ListaServicio.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.lista;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.model.Servicio;
+
+
+public class ListaServicio {
+		private List<Servicio> servicios;
+		public ListaServicio() {
+			servicios = new ArrayList<Servicio>();
+			servicios.add(new Servicio("Lunes", "Juan Perez", "9 a 12 - 16 a 18"));
+			servicios.add(new Servicio("Martes","Lucas Diaz", "9 a 12 - 16 a 18"));
+			servicios.add(new Servicio("Jueves","Maria Gomez", "9 a 12 - 16 a 18"));
+		}
+		public List<Servicio> getServicios(){
+			return servicios;
+		}
+		public void setServicios(List<Servicio> servicios) {
+			this.servicios = servicios;
+		
+		}
+		
+
+}
