d0addc16003f2b02ab7312a26f49560d9894a25d
eveli <eveli@DESKTOP-FMF8N0N>
2023-06-20 20:33:31 -0300

    Se modificó el archivo consejo.java del paquete entity.

--- tp2_Grupo24/src/main/java/ar/edu/unju/fi/entity/Consejo.java
+++ tp2_Grupo24/src/main/java/ar/edu/unju/fi/entity/Consejo.java
@@ -2,19 +2,43 @@ package ar.edu.unju.fi.entity;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 /**
  * Clase que representa un consejo.
  */
 @Component
+@Entity
+@Table(name="consejos")
 public class Consejo {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_consejo")
+	private Long id;
+	
+	@Column(name="titulo_consejo")
 	@NotEmpty(message="*Debe de Agregar el titulo")
+	@Size(min=4,message="*Titulo muy corto")
     private String titulo;
+	
+	@Column(name="descripcion_consejo")
 	@NotEmpty(message="*Debe de Agregar descripcion")
+	@Size(min=4,message="*Descripcion muy corta")
     private String descripcion;
     
+	@Column(name="imagen_consejo")
     private String imagen;
+	
+	@Column(name="estado_consejo")
+	private boolean estado;
 
     /**
      * Constructor por defecto de la clase Consejo.
@@ -25,17 +49,36 @@ public class Consejo {
 
     /**
      * Constructor de la clase Consejo que recibe el título, la descripción y la imagen.
-     *
+     * 
+     * @param id identificador del consejo.
      * @param titulo      el título del consejo.
      * @param descripcion la descripción del consejo.
      * @param imagen      la imagen asociada al consejo.
+     * @param estado estado del consejo.
      */
-    public Consejo(String titulo, String descripcion, String imagen) {
+    public Consejo(Long id, String titulo, String descripcion, String imagen, boolean estado) {
+    	this.id = id;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.imagen = imagen;
+        this.estado = estado;
     }
-
+    
+    /**
+	 * Permite la salida del ID 
+	 * @return retorna el ID del consejo
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * Permite la entrada del atributo ID
+	 * @param id, valor a asignar al ID del objeto
+	 */
+	public void setid(Long id) {
+		this.id = id;
+	}
+    
     /**
      * Obtiene el título del consejo.
      *
@@ -89,4 +132,20 @@ public class Consejo {
     public void setImagen(String imagen) {
         this.imagen = imagen;
     }
+    
+	/**
+	 * Permite la salida del estado
+	 * @return retorna el estado del consejo
+	 */
+    public boolean getEstado() {
+		return estado;
+	}
+    /**
+	 * Permite la entrada del atributo estado
+	 * @param estado, valor a asignar al  estado del objeto
+	 */
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+		
+	}
 }
