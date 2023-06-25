4cb988289e438a7b302b6c3ef95886e6db79f938
David Martinez <David@DESKTOP-MHN5J54>
2023-06-24 23:49:32 -0300

    Se realizó la documentación interna.

--- tp2_Grupo24/src/main/java/ar/edu/unju/fi/repository/IProductoRepository.java
+++ tp2_Grupo24/src/main/java/ar/edu/unju/fi/repository/IProductoRepository.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 
 import ar.edu.unju.fi.entity.Producto;
 @Repository
+/**
+ * Interfaz que define las operaciones de acceso a datos para la entidad Producto.
+ */
 public interface IProductoRepository extends CrudRepository<Producto, Long>{
 	
 	public List<Producto> findByEstado(boolean estado);
--- tp2_Grupo24/src/main/java/ar/edu/unju/fi/service/imp/ProductoServiceMysqlImp.java
+++ tp2_Grupo24/src/main/java/ar/edu/unju/fi/service/imp/ProductoServiceMysqlImp.java
@@ -6,40 +6,62 @@ import org.springframework.stereotype.Service;
 import ar.edu.unju.fi.entity.Producto;
 import ar.edu.unju.fi.repository.IProductoRepository;
 import ar.edu.unju.fi.service.IProductoService;
-
+/**
+ * Implementación de la interfaz IProductoService que define los métodos de servicio para la entidad Producto.
+ * Utiliza el repositorio IProductoRepository para acceder y manipular los datos.
+ */
 @Service("productoServiceMysql")
 public class ProductoServiceMysqlImp implements IProductoService {
 	@Autowired
 	private IProductoRepository productoRepository;
 	@Autowired
 	private Producto producto;
-	
+	  /**
+     * Obtiene una lista de productos, pero solo los que contiene el estado true
+     * @return una lista de productos activos
+     */
 	@Override
 	public List<Producto> getLista() {
 		return productoRepository.findByEstado(true);
 	}
-
+	/**
+     * Guarda un producto en la base de datos.
+     * @param producto el producto a guardar
+     */
 	@Override
 	public void guardar(Producto producto) {
 		productoRepository.save(producto);
 	}
-
+	/**
+     * Modifica un producto existente en la base de datos.
+     * @param producto el producto a modificar
+     */
 	@Override
 	public void modificar(Producto producto) {
 		productoRepository.save(producto);
 	}
-
+	/**
+     * Elimina un producto mediante una eliminación lógica esta establece el estado de un producto en "false".
+     * @param productoEncontrado el producto a eliminar
+     */
 	@Override
 	public void eliminar(Producto productoEncontrado){
 		productoEncontrado.setEstado(false);
 		productoRepository.save(productoEncontrado);
 	}
-
+	 /**
+     * Obtiene un producto por su identificador.
+     * @param id el identificador del producto
+     * @return el producto encontrado
+     */
 	@Override
 	public Producto getBy(Long id) {
 		return productoRepository.findById(id).get();
 	}
-
+	/**
+    * Genera un nuevo producto.
+    * @return un objeto producto.
+    */
 	@Override
 	public Producto getProducto() {
 		return producto;
